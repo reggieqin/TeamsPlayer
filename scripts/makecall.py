a0b4bc9cd63644de6578130b0e1c9d0b7f94f7f5
Reggie Qin <reqqin@cisco.com>
2019-10-08 14:38:23 +0800

    Call remote

--- scripts/makecall.py
+++ scripts/makecall.py
@@ -2,23 +2,45 @@ import requests
 import sys
 import json
 
+def makecall(host, destination):
+    url = 'http://{}:8000/makecall'.format(host)
+    response = requests.post(url, json=destination)
+    if response.status_code != 202:
+        print "Failed to makecall"
+        return False
+
+    return True
+
 def execute(host, params):
     try:
         destination = json.loads(params)
 
-        url = 'http://{}:8000/makecall'.format(host)
-        response = requests.post(url, json=destination)
-        if response.status_code != 202:
-            print "Failed to makecall"
-            return False
+        if 'uri' in destination:
+            return makecall(host, destination)
+        elif 'host' in destination:
+            url = 'http://{}:8000/loggedinuser'.format(destination['host'])
+            response = requests.get(url)
+            if response.status_code != 200:
+                print "Failed to retrieve logged in user from {}".format(destination['host'])
+                return False
 
-        return True
+            r = response.json()
+            if r['email'] is None:
+                print "Invalid email"
+                return False
+
+            data = {}
+            data['uri'] = r['email']
+            return makecall(destination['host'], data)
+        else:
+            print "Invalid params"
+            return False
     except Exception as e:
         print e
         return False
 
 if __name__ == '__main__':
-    if execute('127.0.0.1', '{"uri":"regqin@cisco.com"}'):
+    if execute('127.0.0.1', '{"host":"127.0.0.1"}'):
         print "Succeed"
     else:
         print "Failed"
